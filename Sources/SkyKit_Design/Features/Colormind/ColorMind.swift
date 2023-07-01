931c074d176ad64e6ca027f14a35b0afcec9c62a
Adélaïde Sky <augustin.humez@gmail.com>
2023-07-01 22:37:56 +0200

    generate -> regenerate

--- Sources/SkyKit_Design/Features/Colormind/ColorMind.swift
+++ Sources/SkyKit_Design/Features/Colormind/ColorMind.swift
@@ -69,7 +69,7 @@ public class SKColorMind: ObservableObject {
 }
 
 public extension SKColorMind {
-    func generate(using model: String = SKColorMind.shared.model) {
+    func regenerate(using model: String = SKColorMind.shared.model) {
         self.model = model
         let request = AF.request(self.apiURL, method: .post, parameters: self.body, encoding: JSONEncoding.default)
             .validate()
@@ -80,17 +80,20 @@ public extension SKColorMind {
                 }
                 
                 do {
+                    var tmpColors: [Colors] = []
+                    
                     var json = try? JSON(data: response.data!)
                     for color in json!["result"].arrayValue {
                         
                         #if os(macOS)
                         let color = NSColor(red: CGFloat(color[0].floatValue), green: CGFloat(color[1].floatValue), blue: CGFloat(color[2].floatValue), alpha: CGFloat(1))
-                        self.palette.append(Color(nsColor: color))
+                        tmpColors.append(Color(nsColor: color))
                         #else
                         let color = UIColor(red: CGFloat(color[0].floatValue/255), green: CGFloat(color[1].floatValue/255), blue: CGFloat(color[2].floatValue/255), alpha: CGFloat(1))
-                        self.palette.append(Color(uiColor: color))
+                        tmpColors.append(Color(uiColor: color))
                         #endif
                     }
+                    self.palette = tmpColors
                 }
             }
     }
