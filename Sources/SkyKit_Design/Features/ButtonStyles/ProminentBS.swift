229349b65964be78e9c2ee598ad50fac3c368e15
Adélaïde Sky <augustin.humez@gmail.com>
2023-09-21 21:10:29 +0200

    gnam

--- Sources/SkyKit_Design/Features/ButtonStyles/ProminentBS.swift
+++ Sources/SkyKit_Design/Features/ButtonStyles/ProminentBS.swift
@@ -38,3 +38,38 @@ public struct ProminentButtonStyle: ButtonStyle {
 public extension ButtonStyle where Self == ProminentButtonStyle {
     static var prominent: Self { Self() }
 }
+
+public struct ProminentShadowedButtonStyle: ButtonStyle {
+    @Environment(\.colorScheme) private var colorScheme
+    @Environment(\.isEnabled) private var isEnabled
+    let darkStart = Color(red: 50 / 255, green: 60 / 255, blue: 65 / 255)
+    let darkEnd = Color(red: 25 / 255, green: 25 / 255, blue: 30 / 255)
+    public func makeBody(configuration: Self.Configuration) -> some View {
+        HStack {
+            Spacer()
+            configuration.label
+                .font(.headline)
+                .padding(15)
+            Spacer()
+        }
+            .background {
+                ZStack {
+                    Rectangle()
+                        .fill()
+                        .foregroundStyle(.tint.shadow(.inner(color: .gray.opacity(0.2), radius: 5, x: 5, y: 5)).shadow(.inner(color: .black.opacity(0.2), radius: 5, x: -5, y: -5)))
+                        .opacity(isEnabled ? 1 : 0)
+                    SKNoiseTexture()
+                        .opacity(0.1)
+                }
+                    .opacity(configuration.isPressed ? 0.8 : 1)
+            }
+            .cornerRadius(10)
+            .symbolVariant(configuration.isPressed ? .fill : .none)
+            
+            
+    }
+}
+
+public extension ButtonStyle where Self == ProminentShadowedButtonStyle {
+    static var prominentShadowed: Self { Self() }
+}
