0801d846f80882c911b663a09dcff23c8394061e
Adélaïde Sky <augustin.humez@gmail.com>
2023-09-21 20:30:46 +0200

    uwsdlfkjh

--- Sources/SkyKit_Design/Features/Colorful/NuancedColorView.swift
+++ Sources/SkyKit_Design/Features/Colorful/NuancedColorView.swift
@@ -1,1 +1,257 @@
+//
+//  ColorfulView.swift
+//  Colorful
+//
+//  Created by Lakr Aream on 2021/9/19.
+//
+import SwiftUI
 
+public struct SKNuancedColorfulView: View {
+    // MARK: - PROPERTY
+
+    @State var randomization: [PointRandomization]
+    @State var size: CGSize = .init()
+
+    var color: Color
+    private let animation: Animation
+    private let blurRadius: CGFloat
+    
+    @State private var updating = true
+
+    private let timer = Timer
+        .publish(every: 5, on: .main, in: .common)
+        .autoconnect()
+
+    // MARK: - INIT
+
+    public init(
+        _ basecolor: Color = .red,
+        animation: Animation = .bouncy,
+        blurRadius: CGFloat = 1,
+        amount: Int = 32
+    ) {
+        assert(blurRadius > 0)
+        assert(amount > 0)
+
+        self.animation = animation
+        self.blurRadius = blurRadius
+
+        
+        self.color = basecolor
+
+        var builder = [PointRandomization]()
+        for _ in 0 ..< amount {
+            builder.append(.init())
+        }
+        _randomization = State(initialValue: builder)
+    }
+    // MARK: - VIEW
+
+    public var body: some View {
+        GeometryReader { reader in
+            ZStack {
+                ForEach(obtainRangeAndUpdate(size: reader.size)) { configure in
+                    Circle()
+                        .foregroundColor(configure.nuance(color))
+                        .animation(.easeInOut(duration: 1), value: color)
+                        .opacity(0.5)
+                        .frame(
+                            width: configure.diameter,
+                            height: configure.diameter
+                        )
+                        .offset(
+                            x: configure.offsetX,
+                            y: configure.offsetY
+                        )
+                }
+            }
+            .frame(width: reader.size.width,
+                   height: reader.size.height)
+        }
+        .clipped()
+        .blur(radius: blurRadius)
+        .onReceive(timer) { _ in
+            dispatchUpdate()
+        }
+        .onChange(of: color) { _ in
+            withAnimation(Animation
+                .interpolatingSpring(stiffness: 20, damping: 1)
+                .speed(0.2)) {
+                randomizationStart()
+            }
+        }
+        .onAppear {
+            var randomizationBuilder = [PointRandomization]()
+            for i in 0 ..< randomization.count {
+                let randomizationElement: PointRandomization = {
+                    var builder = PointRandomization()
+                    builder.randomizeIn(size: size)
+                    builder.id = randomization[i].id
+                    return builder
+                }()
+                randomizationBuilder.append(randomizationElement)
+            }
+            randomization = randomizationBuilder
+        }
+    }
+
+    // MARK: - FUNCTION
+
+    private func dispatchUpdate() {
+        withAnimation(animation) {
+            randomizationStart()
+        }
+    }
+
+    private func randomizationStart() {
+        if updating {
+            updating = false
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                self.updating = true
+            }
+            var randomizationBuilder = [PointRandomization]()
+            for i in 0 ..< randomization.count {
+                let randomizationElement: PointRandomization = {
+                    var builder = PointRandomization()
+                    builder.randomizeIn(size: size)
+                    builder.id = randomization[i].id
+                    return builder
+                }()
+                randomizationBuilder.append(randomizationElement)
+            }
+            randomization = randomizationBuilder
+        }
+    }
+
+    private func obtainRangeAndUpdate(size: CGSize) -> [PointRandomization] {
+        issueSizeUpdate(withValue: size)
+        return randomization
+    }
+
+    private func issueSizeUpdate(withValue size: CGSize) {
+        if self.size == size { return }
+        DispatchQueue.main.async {
+            self.size = size
+            self.dispatchUpdate()
+        }
+    }
+}
+
+#if os(iOS)
+
+extension UIColor {
+    private func makeColor(componentDelta: CGFloat) -> UIColor {
+        var red: CGFloat = 0
+        var blue: CGFloat = 0
+        var green: CGFloat = 0
+        var alpha: CGFloat = 0
+        
+        // Extract r,g,b,a components from the
+        // current UIColor
+        getRed(
+            &red,
+            green: &green,
+            blue: &blue,
+            alpha: &alpha
+        )
+        
+        // Create a new UIColor modifying each component
+        // by componentDelta, making the new UIColor either
+        // lighter or darker.
+        return UIColor(
+            red: add(componentDelta, toComponent: red),
+            green: add(componentDelta, toComponent: green),
+            blue: add(componentDelta, toComponent: blue),
+            alpha: alpha
+        )
+    }
+}
+
+extension UIColor {
+    func lighter(_ componentDelta: CGFloat = 0.1) -> UIColor {
+        return makeColor(componentDelta: componentDelta)
+    }
+    
+    func darker(_ componentDelta: CGFloat = 0.1) -> UIColor {
+        return makeColor(componentDelta: -componentDelta)
+    }
+}
+extension UIColor {
+    // Add value to component ensuring the result is
+    // between 0 and 1
+    private func add(_ value: CGFloat, toComponent: CGFloat) -> CGFloat {
+        return max(0, min(1, toComponent + value))
+    }
+}
+
+extension Color {
+    func lighter(_ componentDelta: CGFloat = 0.1) -> Color {
+        return Color(uiColor: UIColor(self).lighter(componentDelta))
+    }
+    
+    func darker(_ componentDelta: CGFloat = 0.1) -> Color {
+        return Color(uiColor: UIColor(self).darker(componentDelta))
+    }
+}
+#elseif os(macOS)
+
+extension NSColor {
+    private func makeColor(componentDelta: CGFloat) -> NSColor {
+        var red: CGFloat = 0
+        var blue: CGFloat = 0
+        var green: CGFloat = 0
+        var alpha: CGFloat = 0
+        
+        guard let nsColor = self.usingColorSpace(.deviceRGB) else {
+            return .clear
+        }
+        
+        // Extract r,g,b,a components from the
+        // current UIColor
+        nsColor.getRed(
+            &red,
+            green: &green,
+            blue: &blue,
+            alpha: &alpha
+        )
+        
+        // Create a new UIColor modifying each component
+        // by componentDelta, making the new UIColor either
+        // lighter or darker.
+        return NSColor(
+            red: add(componentDelta, toComponent: red),
+            green: add(componentDelta, toComponent: green),
+            blue: add(componentDelta, toComponent: blue),
+            alpha: alpha
+        )
+    }
+}
+
+extension NSColor {
+    func lighter(_ componentDelta: CGFloat = 0.1) -> NSColor {
+        return makeColor(componentDelta: componentDelta)
+    }
+    
+    func darker(_ componentDelta: CGFloat = 0.1) -> NSColor {
+        return makeColor(componentDelta: -componentDelta)
+    }
+}
+extension NSColor {
+    // Add value to component ensuring the result is
+    // between 0 and 1
+    private func add(_ value: CGFloat, toComponent: CGFloat) -> CGFloat {
+        return max(0, min(1, toComponent + value))
+    }
+}
+
+extension Color {
+    func lighter(_ componentDelta: CGFloat = 0.1) -> Color {
+        return Color(nsColor: NSColor(self).lighter(componentDelta))
+    }
+    
+    func darker(_ componentDelta: CGFloat = 0.1) -> Color {
+        return Color(nsColor: NSColor(self).darker(componentDelta))
+    }
+}
+
+#endif
--- Sources/SkyKit_Design/Features/Colorful/PointRandomization.swift
+++ Sources/SkyKit_Design/Features/Colorful/PointRandomization.swift
@@ -41,3 +41,66 @@ extension SKColorfulView {
         }
     }
 }
+extension SKNuancedColorfulView {
+    struct PointRandomization: Equatable, Hashable, Identifiable {
+        var id = UUID()
+
+        var diameter: CGFloat = 0
+        var offsetX: CGFloat = 0
+        var offsetY: CGFloat = 0
+        var variant: Variant = .normal
+
+        mutating func randomizeIn(size: CGSize) {
+            let decision = (size.width + size.height) / 4
+            diameter = CGFloat.random(in: (decision * 0.25) ... (decision * 0.75))
+            offsetX = CGFloat.random(in: -(size.width / 2) ... +(size.width / 2))
+            offsetY = CGFloat.random(in: -(size.height / 2) ... +(size.height / 2))
+        }
+        
+        mutating func set(from: PointRandomization) {
+            diameter = from.diameter
+            offsetX = from.offsetX
+            offsetY = from.offsetY
+            variant = from.variant
+        }
+
+        func hash(into hasher: inout Hasher) {
+            hasher.combine(diameter)
+            hasher.combine(offsetX)
+            hasher.combine(offsetY)
+        }
+
+        static func == (lhs: PointRandomization, rhs: PointRandomization) -> Bool {
+            lhs.diameter == rhs.diameter &&
+                lhs.offsetX == rhs.offsetX &&
+                lhs.offsetY == rhs.offsetY
+        }
+        
+        func nuance(_ color: Color) -> Color {
+            switch self.variant {
+            case .darker:
+                return color.darker(0.07)
+            case .dark:
+                return color.darker(0.03)
+            case .normal:
+                return color
+            case .light:
+                return color.lighter(0.15)
+            case .lighter:
+                return color.lighter(0.25)
+            }
+        }
+        
+        init() {
+            variant = Variant.allCases.randomElement()!
+        }
+        
+        enum Variant: CaseIterable {
+            case darker
+            case dark
+            case normal
+            case light
+            case lighter
+        }
+    }
+}
