3f843e4453e3c96f06b32a29251a86dc1a09bf2c
Adélaïde Sky <augustin.humez@gmail.com>
2023-09-21 18:01:21 +0200

    sdfgsfdgs

--- Sources/SkyKit_Design/Features/Colorful/ColorfulView.swift
+++ Sources/SkyKit_Design/Features/Colorful/ColorfulView.swift
@@ -95,7 +95,7 @@ public struct SKColorfulView: View {
                 ForEach(obtainRangeAndUpdate(size: reader.size)) { configure in
                     Circle()
                         .foregroundColor(colorElements.randomElement()!)
-                        .animation(.easeInOut(duration: 0.5), value: colorElements)
+                        .animation(.easeInOut(duration: 2), value: colorElements)
                         .opacity(0.5)
                         .frame(
                             width: configure.diameter,
