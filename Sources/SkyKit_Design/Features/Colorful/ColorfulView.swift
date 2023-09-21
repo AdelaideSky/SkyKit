dd4b368dcf373c817392c9d4e872081cb6e706ad
Adélaïde Sky <augustin.humez@gmail.com>
2023-09-21 18:38:36 +0200

    gfdsdfgs

--- Sources/SkyKit_Design/Features/Colorful/ColorfulView.swift
+++ Sources/SkyKit_Design/Features/Colorful/ColorfulView.swift
@@ -132,7 +132,7 @@ public struct SKColorfulView: View {
         .onChange(of: colorElements) { _ in
             withAnimation(Animation
                 .interpolatingSpring(stiffness: 20, damping: 1)
-                .speed(1)) {
+                .speed(0.2)) {
                 randomizationStart()
             }
         }
