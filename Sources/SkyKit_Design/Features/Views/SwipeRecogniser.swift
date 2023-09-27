51f0c4b625e98570e1723d05992c08c5d48a742f
Adélaïde Sky <augustin.humez@gmail.com>
2023-09-27 13:19:17 +0200

    sgh

--- Sources/SkyKit_Design/Features/Views/SwipeRecogniser.swift
+++ Sources/SkyKit_Design/Features/Views/SwipeRecogniser.swift
@@ -7,12 +7,46 @@
 
 import SwiftUI
 
-struct SwiftUIView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct SKSwipeRecogniser: ViewModifier {
+    var left: () -> () = {}
+    var right: () -> () = {}
+    var up: () -> () = {}
+    var down: () -> () = {}
+    
+    var minDistance: CGFloat
+    
+    init(left: @escaping () -> Void = {}, right: @escaping () -> Void = {}, up: @escaping () -> Void = {}, down: @escaping () -> Void = {}, minimumDistance: CGFloat = 3) {
+        self.left = left
+        self.right = right
+        self.up = up
+        self.down = down
+        self.minDistance = minimumDistance
+    }
+    
+    func body(content: Content) -> some View {
+        content
+            .gesture(DragGesture(minimumDistance: minDistance, coordinateSpace: .local)
+                .onEnded { value in
+                    switch(value.translation.width, value.translation.height) {
+                    case (...0, -30...30):
+                        left()
+                    case (0..., -30...30):
+                        right()
+                    case (-100...100, ...0):
+                        up()
+                    case (-100...100, 0...):
+                        down()
+                    default:
+                        break
+                    }
+                }
+            )
     }
 }
 
-#Preview {
-    SwiftUIView()
+public extension View {
+    func onSwipe(left: @escaping () -> Void = {}, right: @escaping () -> Void = {}, up: @escaping () -> Void = {}, down: @escaping () -> Void = {}, minimumDistance: CGFloat = 3) -> some View {
+        self
+            .modifier(SKSwipeRecogniser(left: left, right: right, up: up, down: down, minimumDistance: minimumDistance))
+    }
 }
