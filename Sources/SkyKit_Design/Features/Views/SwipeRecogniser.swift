a6fb2f38b23257166a30664959b8af818364c75d
Adélaïde Sky <augustin.humez@gmail.com>
2023-09-27 13:18:27 +0200

    sgfhsghf

---
+++ Sources/SkyKit_Design/Features/Views/SwipeRecogniser.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Adélaïde Sky on 27/09/2023.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
