1b81a691e0eebce396599c49a6582cbb3fe231d9
Adélaïde Sky <augustin.humez@gmail.com>
2023-10-17 18:21:52 +0200

    sfgdhfj;g

--- Package.swift
+++ Package.swift
@@ -11,8 +11,11 @@ let package = Package(
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
+//        .library(
+//            name: "SkyKit_Design",
+//            targets: ["SkyKit_Design"]),
         .library(
-            name: "SkyKit_Design",
+            name: "SkyKit",
             targets: ["SkyKit_Design"]),
         .library(
             name: "SkyKit_Charts",
