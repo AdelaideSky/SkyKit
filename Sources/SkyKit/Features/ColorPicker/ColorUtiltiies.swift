4fa3a514524c153ff10fc78c48125abefb26dc04
Adélaïde Sky <augustin.humez@gmail.com>
2023-11-19 16:03:54 +0100

    qfdhsgfjhd

--- Sources/SkyKit/Features/ColorPicker/ColorUtiltiies.swift
+++ Sources/SkyKit/Features/ColorPicker/ColorUtiltiies.swift
@@ -43,4 +43,39 @@ extension Color {
         }
     }
 }
+#else
+extension Color {
+//    func getHSB() -> (CGFloat, CGFloat, CGFloat) {
+//        autoreleasepool {
+//            guard let nsColor = NSColor(self).usingColorSpace(.deviceRGB) else {
+//                return (0, 0, 0)
+//            }
+//            
+//            var hue: CGFloat = 0
+//            var saturation: CGFloat = 0
+//            var brightness: CGFloat = 0
+//            var alpha: CGFloat = 0
+//            
+//            nsColor.getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: &alpha)
+//            
+//            return (hue, saturation, brightness)
+//        }
+//    }
+    func getRGB() -> (CGFloat, CGFloat, CGFloat) {
+        autoreleasepool {
+            guard let uiColor = UIColor(self) else {
+                return (0, 0, 0)
+            }
+            
+            var red: CGFloat = 0
+            var green: CGFloat = 0
+            var blue: CGFloat = 0
+            var alpha: CGFloat = 0
+            
+            uiColor.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+            
+            return (red, green, blue)
+        }
+    }
+}
 #endif
