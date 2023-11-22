dc4898825e417128c64ef719ec1556a468f65c6f
Adélaïde Sky <augustin.humez@gmail.com>
2023-11-22 16:25:50 +0100

    qddfshfgdjhf,

--- Sources/SkyKit/Features/CircleImagePicker/ImagePicker.swift
+++ Sources/SkyKit/Features/CircleImagePicker/ImagePicker.swift
@@ -126,16 +126,14 @@ struct CropView: View {
                     .scaleEffect(scale)
                     .offset(offset)
                     .mask(
-                        Group {
-                            switch shape {
-                            case .circle:
-                                Circle()
-                                    .frame(width: maskRadius * 2, height: maskRadius * 2)
-                            case .square:
-                                RoundedRectangle(cornerRadius: 10) {
-                                    .frame(width: maskRadius * 2, height: maskRadius * 2)
-                                }
-                            }
+                        switch shape {
+                        case .circle:
+                            Circle()
+                                .frame(width: maskRadius * 2, height: maskRadius * 2)
+                        case .square:
+                            RoundedRectangle(cornerRadius: 10)
+                                .frame(width: maskRadius * 2, height: maskRadius * 2)
+                            
                         }
                     )
             }
