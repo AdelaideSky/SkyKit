fe67468d403d6db382c52ab1592da83b95496393
0xilis <109871561+0xilis@users.noreply.github.com>
2023-12-15 17:48:47 -0500

    Optim: Save a mod

--- Sources/SkyKitC/random.c
+++ Sources/SkyKitC/random.c
@@ -16,7 +16,18 @@ int *randomAlpha(int n) {
     srand(time(NULL));
     for (i = 0; i < n; i++)
     {
-        array[i] = (255 << 24) | (255 << 16) | (255 << 8) | rand() % 256;
+        /* 
+         * here, we are generating a RGBA color
+         * with R 255, G 255, B 255, and a random
+         * A (I mean the function is called
+         * randomAlpha, what did you expect,
+         * a random red?). Counting the A value,
+         * it is a 32 bit color, and only the last
+         * 8 bits of the color we're generating.
+         * So basically, last 8 bits of rand(),
+         * but set all other bits.
+        */
+        array[i] = 0xFFFFFF00 | rand();
     }
     return array;
 }
