36eb430ce21e30f1fbbb29b71aabd022006a9346
Adélaïde Sky <augustin.humez@gmail.com>
2024-01-12 19:17:32 +0100

    qsdgfdh

--- Sources/SkyKit/Features/Views/Glow.swift
+++ Sources/SkyKit/Features/Views/Glow.swift
@@ -8,17 +8,24 @@
 import SwiftUI
 
 public struct GlowViewModifier: ViewModifier {
+    let radius: CGFloat
+    let opacity: CGFloat
+    init(radius: CGFloat = 4, opacity: CGFloat = 1) {
+        self.radius = radius
+        self.opacity = opacity
+    }
     public func body(content: Content) -> some View {
         content
             .background(
                 content
-                    .blur(radius: 4)
+                    .blur(radius: radius)
+                    .opacity(opacity)
             )
     }
 }
 
 public extension View {
-    func glow() -> some View {
-        self.modifier(GlowViewModifier())
+    func glow(radius: CGFloat = 4, opacity: CGFloat = 1) -> some View {
+        self.modifier(GlowViewModifier(radius: radius, opacity: opacity))
     }
 }
