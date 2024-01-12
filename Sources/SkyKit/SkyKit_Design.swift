9eea22236b4a40f38b644163c05e750be2a476aa
Adélaïde Sky <augustin.humez@gmail.com>
2024-01-12 18:54:22 +0100

    qgdfhsgjd

--- Sources/SkyKit/SkyKit_Design.swift
+++ Sources/SkyKit/SkyKit_Design.swift
@@ -1,6 +1,19 @@
-public struct SKDesign {
-    public private(set) var text = "Hello, World!"
+import SwiftUI
 
-    public init() {
+@available(iOS 17, macOS 14, macCatalyst 17, tvOS 17, visionOS 1, *)
+@dynamicMemberLookup
+public enum SkyKitShaders {
+    /// Returns a new shader function representing the stitchable MSL
+    /// function called `name` in the Inferno shader library.
+    ///
+    /// Typically this subscript is used implicitly via the dynamic
+    /// member syntax, for example:
+    ///
+    ///    let fn = InfernoShaderLibrary.myFunction
+    ///
+    /// which creates a reference to the MSL function called
+    /// `myFunction()`.
+    public static subscript(dynamicMember name: String) -> ShaderFunction {
+        ShaderLibrary.bundle(.module)[dynamicMember: name]
     }
 }
