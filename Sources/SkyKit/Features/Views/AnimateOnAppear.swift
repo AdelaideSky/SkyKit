43ba69e12c5d8ba5fc54730b8e064643a148b10f
Adélaïde Sky <augustin.humez@gmail.com>
2024-02-04 17:26:55 +0100

    fdsgdnhf,j

---
+++ Sources/SkyKit/Features/Views/AnimateOnAppear.swift
@@ -0,0 +1,26 @@
+//
+//  AnimateOnAppear.swift
+//
+//
+//  Created by Adélaïde Sky on 04/02/2024.
+//
+
+import SwiftUI
+
+struct AnimateOnAppearModifier: ViewModifier {
+    let animation: Animation
+    
+    @State var animate: Bool = false
+    
+    func body(content: Content) -> some View {
+        content
+            .onAppear { animate.toggle() }
+            .animation(animation, value: animate)
+    }
+}
+
+extension View {
+    func appearAnimation(_ animation: Animation) -> some View {
+        modifier(AnimateOnAppearModifier(animation: animation))
+    }
+}
