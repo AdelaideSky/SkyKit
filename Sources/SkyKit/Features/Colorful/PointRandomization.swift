dbc3093f5f70ff30924d0a5480fe7923e510cc83
Adélaïde Sky <augustin.humez@gmail.com>
2024-02-06 18:06:35 +0100

    fxdgchvbjknlmù

--- Sources/SkyKit/Features/Colorful/PointRandomization.swift
+++ Sources/SkyKit/Features/Colorful/PointRandomization.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Observation
 
 extension SKColorfulView {
     struct PointRandomization: Equatable, Hashable, Identifiable {
@@ -41,66 +42,94 @@ extension SKColorfulView {
         }
     }
 }
-extension SKNuancedColorfulView {
-    struct PointRandomization: Equatable, Hashable, Identifiable {
-        var id = UUID()
 
-        var diameter: CGFloat = 0
-        var offsetX: CGFloat = 0
-        var offsetY: CGFloat = 0
-        var variant: Variant = .normal
+class PointRandomization: Equatable, Hashable, Identifiable {
+    var id = UUID()
 
-        mutating func randomizeIn(size: CGSize) async {
-            let decision = (size.width + size.height) / 4
-            diameter = CGFloat.random(in: (decision * 0.25) ... (decision * 0.75))
-            offsetX = CGFloat.random(in: -(size.width / 2) ... +(size.width / 2))
-            offsetY = CGFloat.random(in: -(size.height / 2) ... +(size.height / 2))
+    var diameter: CGFloat = 0
+    var offsetX: CGFloat = 0
+    var offsetY: CGFloat = 0
+    var variant: Variant = .normal
+
+    func randomizeIn(size: CGSize) async {
+        let decision = (size.width + size.height) / 4
+        diameter = CGFloat.random(in: (decision * 0.25) ... (decision * 0.75))
+        offsetX = CGFloat.random(in: -(size.width / 2) ... +(size.width / 2))
+        offsetY = CGFloat.random(in: -(size.height / 2) ... +(size.height / 2))
+        
+    }
+    
+    func set(from: PointRandomization) {
+        diameter = from.diameter
+        offsetX = from.offsetX
+        offsetY = from.offsetY
+        variant = from.variant
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(diameter)
+        hasher.combine(offsetX)
+        hasher.combine(offsetY)
+    }
+
+    static func == (lhs: PointRandomization, rhs: PointRandomization) -> Bool {
+        lhs.diameter == rhs.diameter &&
+            lhs.offsetX == rhs.offsetX &&
+            lhs.offsetY == rhs.offsetY
+    }
+    
+    func nuance(_ color: Color) -> Color {
+        switch self.variant {
+        case .darker:
+            return color.darker(0.07)
+        case .dark:
+            return color.darker(0.03)
+        case .normal:
+            return color
+        case .light:
+            return color.lighter(0.15)
+        case .lighter:
+            return color.lighter(0.25)
+        }
+    }
+    
+    init() {
+        variant = Variant.allCases.randomElement()!
+    }
+    
+    enum Variant: CaseIterable {
+        case darker
+        case dark
+        case normal
+        case light
+        case lighter
+    }
+}
+
+@Observable
+class RandomizationManager {
+    var points: [PointRandomization]
+    var count: Int
+    
+    init(count: Int) {
+        self.count = count
+        
+        var builder = [PointRandomization]()
+        for _ in 0 ..< count {
+            builder.append(.init())
         }
         
-        mutating func set(from: PointRandomization) {
-            diameter = from.diameter
-            offsetX = from.offsetX
-            offsetY = from.offsetY
-            variant = from.variant
-        }
-
-        func hash(into hasher: inout Hasher) {
-            hasher.combine(diameter)
-            hasher.combine(offsetX)
-            hasher.combine(offsetY)
-        }
-
-        static func == (lhs: PointRandomization, rhs: PointRandomization) -> Bool {
-            lhs.diameter == rhs.diameter &&
-                lhs.offsetX == rhs.offsetX &&
-                lhs.offsetY == rhs.offsetY
+        points = builder
+    }
+    
+    func reroll(_ size: CGSize, withAnimation animation: Animation? = nil) async {
+        let builder = points
+        for index in 0...count-1 {
+            await builder[index].randomizeIn(size: size)
         }
         
-        func nuance(_ color: Color) -> Color {
-            switch self.variant {
-            case .darker:
-                return color.darker(0.07)
-            case .dark:
-                return color.darker(0.03)
-            case .normal:
-                return color
-            case .light:
-                return color.lighter(0.15)
-            case .lighter:
-                return color.lighter(0.25)
-            }
-        }
-        
-        init() {
-            variant = Variant.allCases.randomElement()!
-        }
-        
-        enum Variant: CaseIterable {
-            case darker
-            case dark
-            case normal
-            case light
-            case lighter
+        withAnimation(animation) {
+            points = builder
         }
     }
 }
