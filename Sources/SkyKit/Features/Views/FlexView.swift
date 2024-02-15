1eaa854159b1c41911a6b693332aaaa33891271c
Adélaïde Sky <augustin.humez@gmail.com>
2024-02-15 22:16:53 +0100

    fdcghvbjknl,m

--- Sources/SkyKit/Features/Views/FlexView.swift
+++ Sources/SkyKit/Features/Views/FlexView.swift
@@ -32,66 +32,116 @@ public struct SKFlexibleView<Data: RandomAccessCollection, Content: View>: View 
 }
 
 @available(iOS 16.0, *)
-public struct SKFlexHStack: Layout {
+struct SKFlexHStack : Layout {
+    
+    var alignment: HorizontalAlignment = .center
     let horizontalSpacing: Double
     let verticalSpacing: Double
     
-    public init(horizontalSpacing: Double, verticalSpacing: Double) {
+    public init(horizontalSpacing: Double, verticalSpacing: Double, alignment: HorizontalAlignment = .center) {
         self.horizontalSpacing = horizontalSpacing
         self.verticalSpacing = verticalSpacing
+        self.alignment = alignment
     }
     
-    public init(spacing: Double) {
+    public init(spacing: Double, alignment: HorizontalAlignment = .center) {
         self.horizontalSpacing = spacing
         self.verticalSpacing = spacing
+        self.alignment = alignment
         
     }
 
-    public func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) -> CGSize {
-        let nbRows = Double(calculateNumberOrRow(for: subviews, with: proposal.width!))
-        let minHeight = subviews.map { $0.sizeThatFits(proposal).height }.reduce(0) { max($0, $1).rounded(.up) }
-        let height = nbRows * minHeight + max(nbRows - 1, 0) * verticalSpacing
-
-        return CGSize(width: proposal.width!, height: height + 6)
+    func makeCache(subviews: Subviews) -> Cache {
+        return .init(rows: [], height: 0)
     }
-
-    public func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
-        let minHeight = subviews.map { $0.sizeThatFits(proposal).height }.reduce(0) { max($0, $1).rounded(.up) }
-        var pt = CGPoint(x: bounds.minX, y: bounds.minY + 3)
     
-        for subview in subviews.sorted(by: { $0.priority > $1.priority }) {
-            let width = subview.sizeThatFits(proposal).width
-        
-            if (pt.x +  width) > bounds.maxX {
-                pt.x = bounds.minX
-                pt.y += minHeight + verticalSpacing
+    func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout Cache) -> CGSize {
+        let maxWidth = proposal.width ?? 0
+        cache = caculateRows(maxWidth, proposal: proposal, subviews: subviews)
+        return .init(width: maxWidth, height: cache.height)
+    }
+    
+    func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout Cache) {
+        var origin = bounds.origin
+        var subviews = subviews
+        for row in cache.rows {
+            //reset to the row beginning
+            origin.x = getRowXOrigin(bounds: bounds, rowWidth: row.size.width)
+            for size in row.viewsSizes {
+                guard let view = subviews.popFirst() else { return }
+                let width = size.width
+                view.place(at: origin, proposal: .init(size))
+                origin.x += width + horizontalSpacing
             }
-        
-            subview.place(at: pt, anchor: .topLeading, proposal: proposal)
-            pt.x += width + horizontalSpacing
+            //move to the next row
+            origin.y += row.size.height + verticalSpacing
         }
     }
-
-    func calculateNumberOrRow(for subviews: Subviews, with width: Double) -> Int {
-        var nbRows = 0
-        var x: Double = 0
     
-        for subview in subviews {
-            let addedX = subview.sizeThatFits(.unspecified).width + horizontalSpacing
-        
-            let isXWillGoBeyondBounds =  x + addedX > width
-            if isXWillGoBeyondBounds {
-                x = 0
-                nbRows += 1
+    private func getRowXOrigin(bounds: CGRect, rowWidth: CGFloat) -> CGFloat {
+        switch alignment {
+        case .center: return (bounds.minX + bounds.maxX - rowWidth)/2
+        case .trailing: return bounds.maxX - rowWidth
+        default: return bounds.minX
+        }
+    }
+    
+    private func caculateRows(_ maxWidth: CGFloat, proposal: ProposedViewSize, subviews: Subviews) -> Cache {
+        var rows : [Cache.Row] = []
+        var height: CGFloat = 0
+        var subviews = subviews
+        while !subviews.isEmpty {
+            guard let row = calculateRow(maxWidth, proposal: proposal, subviews: &subviews) else { break }
+            rows.append(row)
+            height += row.size.height + verticalSpacing
+        }
+        height -= verticalSpacing
+        return .init(rows: rows, height: height)
+    }
+    
+    
+    private func calculateRow(_ maxWidth: CGFloat, proposal: ProposedViewSize, subviews: inout Subviews) -> Cache.Row? {
+        var viewSizes : [CGSize] = []
+        var rowHeight : CGFloat = 0
+        var origin = CGRect.zero.origin
+        var hasSpace : (CGSize) -> Bool = {(origin.x + $0.width + horizontalSpacing) <= maxWidth}
+        //keep iterating untill row is filled
+        while true {
+            // if no views left
+            //if view size bigger than available space
+            guard
+                let size = subviews.first?.sizeThatFits(proposal),
+                hasSpace(size)
+            else {
+                let rowSize = CGSize(width: origin.x - horizontalSpacing , height: rowHeight)
+                return viewSizes.isEmpty ? nil : .init(viewsSizes: viewSizes, size: rowSize)
             }
+            
+            _ = subviews.popFirst()
+            viewSizes.append(size)
+            rowHeight = rowHeight > size.height ? rowHeight : size.height
+            origin.x += (size.width + horizontalSpacing)
+            
+        }
         
-            x += addedX
-        }
-    
-        if x > 0 {
-            nbRows += 1
-        }
-    
-        return nbRows
+        let rowSize = CGSize(width: origin.x - horizontalSpacing , height: rowHeight)
+        return viewSizes.isEmpty ? nil : .init(viewsSizes: viewSizes, size: rowSize)
     }
+    
+    
+}
+
+
+extension SKFlexHStack {
+    
+    struct Cache {
+        let rows: [Row]
+        let height: CGFloat
+        
+        struct Row {
+            let viewsSizes: [CGSize]
+            let size : CGSize
+        }
+    }
+    
 }
