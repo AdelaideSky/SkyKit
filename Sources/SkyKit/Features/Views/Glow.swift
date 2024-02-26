2f1ddcf5eea1da32f2a7954cd51ba17d367e9967
Adélaïde Sky <augustin.humez@gmail.com>
2024-02-26 19:19:34 +0100

    xbvb

--- Sources/SkyKit/Features/Views/Glow.swift
+++ Sources/SkyKit/Features/Views/Glow.swift
@@ -10,10 +10,17 @@ import SwiftUI
 public struct GlowViewModifier: ViewModifier {
     let radius: CGFloat
     let opacity: CGFloat
-    init(radius: CGFloat = 4, opacity: CGFloat = 1) {
+    
+    let saturation: CGFloat
+    let brightness: CGFloat
+    
+    init(radius: CGFloat = 4, opacity: CGFloat = 1, saturation: CGFloat = 1, brightness: CGFloat = 0) {
         self.radius = radius
         self.opacity = opacity
+        self.saturation = saturation
+        self.brightness = brightness
     }
+    
     public func body(content: Content) -> some View {
         if radius > 0 {
             content
@@ -21,6 +28,8 @@ public struct GlowViewModifier: ViewModifier {
                     content
                         .blur(radius: radius)
                         .opacity(opacity)
+                        .brightness(brightness)
+                        .saturation(saturation)
                 )
         } else {
             content
@@ -29,7 +38,7 @@ public struct GlowViewModifier: ViewModifier {
 }
 
 public extension View {
-    func glow(radius: CGFloat = 4, opacity: CGFloat = 1) -> some View {
-        self.modifier(GlowViewModifier(radius: radius, opacity: opacity))
+    func glow(radius: CGFloat = 4, opacity: CGFloat = 1, saturation: CGFloat = 1, brightness: CGFloat = 0) -> some View {
+        self.modifier(GlowViewModifier(radius: radius, opacity: opacity, saturation: saturation, brightness: brightness))
     }
 }
